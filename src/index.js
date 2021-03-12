55035ab608820209793595bb1e1a20972b8245ea
Liubomyr86 <demianchukliubomyr@gmail.com>
2021-03-12 23:18:59 +0100

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    let brackets = bracketsConfig.map(item => item.join(''));
+
+    while (str.length > 0) {
+        let newStr = str;
+
+        brackets.forEach(item => newStr = newStr.replace(item, ''));
+
+        if (newStr === str)  return false ;
+
+        str = newStr;
+
+    }
+
+    return str.length === 0;
 }
